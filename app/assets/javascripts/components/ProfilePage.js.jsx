8e3382333059f1079a52fd5df48fbd31595b5c70
Tyler Neustaedter <rtneust@gmail.com>
2015-10-31 16:21:13 -0500

    character list display is now functional.

--- app/assets/javascripts/components/ProfilePage.js.jsx
+++ app/assets/javascripts/components/ProfilePage.js.jsx
@@ -1,10 +1,13 @@
  var ProfilePage = React.createClass({
 
  	getInitialState:function(){
- 		return {count: 0, usersData:[]}
+ 		return {count: 0, charactersData:[]}
  	},
 
  	componentWillMount: function(){
+
+ 			console.log('step 1')
+ 		var self = this
 
  		$.ajax(
  			{url: 'api/users',
@@ -18,9 +21,13 @@
  			{url: 'api/characters',
  			dataType: 'json',
  			success: (function(responseData)
- 				{console.log("heres chracters", responseData)
- 				return responseData})
- 				})
+ 				{console.log("heres chracters", responseData.characters.characters)})
+ 				}).then(function(responseData){
+ 					console.log('teenagers.')
+ 					self.setState({charactersData: responseData.characters.characters})
+ 						console.log("more teenages", self.state.charactersData)})
+
+
  	},
 
  	_logOut: function(){
@@ -40,21 +47,24 @@
 
  		return(
  			<CharacterList 
- 				characterlist={this.props.characterlist}
+ 				characterlist={this.state.characterData}
  				character={character}/>
  			)
  	},
 
  	render: function(){
 
- 		var characters = this.props.characterlist
+		var characters = this.state.charactersData 
+		console.log(characters)
 
  		return (
  			<div id="ProfilePage">
  				<button onClick={this._logOut}>Log Out</button>
  				<h1>Welcome current User!</h1>
+ 				<button onClick={this._createCharacter}>Create new Character</button>
  				<div id='characterList'>
 	 				<ul>
+	 					{characters.map(this._showCharacters)}
 	 				</ul>
  				</div>
  				<div id="recentActivity">
