b96128a60b665aa50cbe27ce7d8c72c3a35c4049
Tyler Neustaedter <rtneust@gmail.com>
2015-11-01 16:11:08 -0600

    removes the root stuff for a second time...

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -11,7 +11,6 @@ class Api::SessionsController < ApplicationController
     if user.present? && user.authenticate(password)
       # sign_in user
       session[:user_id] = user.id
-      render root_path
     else
       # render json: ?.errors
     end
