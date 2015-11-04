3a2a616a4d747f57e4e017d71aa4ee5ab9901239
Xavi G <alexandro.x.guerrero@gmail.com>
2015-11-03 20:19:35 -0600

    fix foreign keys and tweak character creation

--- db/migrate/20151102024906_create_capability_blocks.rb
+++ db/migrate/20151102024906_create_capability_blocks.rb
@@ -2,9 +2,6 @@ class CreateCapabilityBlocks < ActiveRecord::Migration
   def change
     create_table :capability_blocks do |t|
       t.integer :character_id
-      t.integer :spell_list_id
-      t.integer :ability_score_block_id
-      t.integer :skill_block_id
 
       t.timestamps null: false
     end
