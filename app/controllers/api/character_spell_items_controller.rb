d867eb399ae1eacf931f235e34b0b9aa45efa86f
Xavi G <alexandro.x.guerrero@gmail.com>
2015-11-11 15:38:55 -0600

    add skill mechanics

--- app/controllers/api/character_spell_items_controller.rb
+++ app/controllers/api/character_spell_items_controller.rb
@@ -1,6 +1,16 @@
 class Api::CharacterSpellItemsController < ApplicationController
-  before_action do
-    @current_character = Character.find_by id: session[:character_id]
+
+  def create
+    @curr_char = Character.find_by id: params[:character_id]
+    @capabilities = @curr_char.capability_block
+    @character_spell_item = @inventory.character_spell_items.new(character_spell_item_params)
+    @capabilities.save
+    render json: @character_spell_item
   end
+
+  private
+    def character_spell_item_params
+      params.require(:character_spell_item).permit(:spell_id)
+    end
 
 end
