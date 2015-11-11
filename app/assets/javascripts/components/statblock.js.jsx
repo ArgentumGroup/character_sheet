36ecc87f4b7a35b6c4c6fe700fdedd30dab52a49
Tyler Neustaedter <rtneust@gmail.com>
2015-11-11 12:03:47 -0600

    fixes the sign in and up pages after setting a width on the container.

--- app/assets/javascripts/components/statblock.js.jsx
+++ app/assets/javascripts/components/statblock.js.jsx
@@ -5,20 +5,26 @@ var StatsBlock = React.createClass({
 		
 		var capabilityBlock = this.props.capabilityBlock;
 		var currentCharacter = this.props.currentCharacter;
+		var intelligence_modifier = currentCharacter.intelligence_modifier;
+		var strength_modifier = currentCharacter.strength_modifier;
+		var dexterity_modifier = currentCharacter.dexterity_modifier;
+		var constitution_modifier = currentCharacter.constitution_modifier;
+		var wisdom_modifier = currentCharacter.wisdom_modifier;
+		var charisma_modifier = currentCharacter.charisma_modifier;
 
 		console.log('heres stats block', capabilityBlock)
 
 		return(
 			<div id='statsBlock'>
-				<p>Profiency Bonus <input value={currentCharacter.proficiency_bonus}ref='proficiency' type='number'/></p>
+				<p>Profiency Bonus <input value={currentCharacter.proficiency_bonus}ref='proficiency' type='string'/></p>
 				<p id='modifier'>Score / Modifier</p>
 				<div>	
-					<p>Strength<input value={capabilityBlock.strength} ref='str' type='number'/></p>
-					<p>Dexterity<input value={capabilityBlock.dexterity}ref='dex' type='number'/></p>
-					<p>Constitution<input value={capabilityBlock.constitution}ref='con' type='number'/></p>
-					<p>Intelligence<input value={currentCharacter.intelligence_modifier}type='string'/><input value={capabilityBlock.intelligence} ref='int' type='number'/></p>
-					<p>Wisdom<input value={capabilityBlock.wisdom}  ref='wis' type='number'/></p>
-					<p>Charisma<input value={capabilityBlock.charisma} ref='cha' type='number'/></p>
+					<p>Strength<input value={strength_modifier} type='string'/><input value={capabilityBlock.strength} ref='str' type='number'/></p>
+					<p>Dexterity<input value={dexterity_modifier} type='string'/><input value={capabilityBlock.dexterity}ref='dex' type='number'/></p>
+					<p>Constitution<input value={constitution_modifier} type='string'/><input value={capabilityBlock.constitution}ref='con' type='number'/></p>
+					<p>Intelligence<input value={intelligence_modifier}type='string'/><input value={capabilityBlock.intelligence} ref='int' type='number'/></p>
+					<p>Wisdom<input value={wisdom_modifier} type='string'/><input value={capabilityBlock.wisdom}  ref='wis' type='number'/></p>
+					<p>Charisma<input value={charisma_modifier} type='string'/><input value={capabilityBlock.charisma} ref='cha' type='number'/></p>
 				</div>	
 			</div>	
 			)
